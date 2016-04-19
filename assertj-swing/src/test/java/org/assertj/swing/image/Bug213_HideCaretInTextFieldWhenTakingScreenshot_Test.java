4ca394fcd3020b0d14e9527b28ef61b34b428acb
Timo Mikkilä <mikkilat@gmail.com>
2016-04-19 17:00:17 +0300

    Fix compare screenshots pixel by pixel instead of fields in BufferedImage objects

--- assertj-swing/src/test/java/org/assertj/swing/image/Bug213_HideCaretInTextFieldWhenTakingScreenshot_Test.java
+++ assertj-swing/src/test/java/org/assertj/swing/image/Bug213_HideCaretInTextFieldWhenTakingScreenshot_Test.java
@@ -51,8 +51,23 @@ public class Bug213_HideCaretInTextFieldWhenTakingScreenshot_Test extends Sequen
     BufferedImage currentImage = screenshotTaker.takeScreenshotOf(window);
     for (int i = 0; i < 100; i++) {
       BufferedImage newImage = screenshotTaker.takeScreenshotOf(window);
-      assertThat(newImage).isEqualTo(currentImage);
+      assertThat(imagesMatch(currentImage, newImage));
     }
+  }
+
+  private boolean imagesMatch(BufferedImage img1, BufferedImage img2) {
+    if (img1.getWidth() == img2.getWidth() && img1.getHeight() == img2.getHeight()) {
+      for (int x = 0; x < img1.getWidth(); x++) {
+        for (int y = 0; y < img1.getHeight(); y++) {
+          if (img1.getRGB(x, y) != img2.getRGB(x, y)) {
+            return false;
+          }
+        }
+      }
+    } else {
+      return false;
+    }
+    return true;
   }
 
   private static class MyWindow extends TestWindow {
