45d762f5838aeb26b682b125752d95012abc2dbd
Timo Mikkilä <mikkilat@gmail.com>
2016-04-21 07:08:33 +0300

    Destroy all windows from hierarchy after test run.

--- assertj-swing/src/test/java/org/assertj/swing/test/core/EDTSafeTestCase.java
+++ assertj-swing/src/test/java/org/assertj/swing/test/core/EDTSafeTestCase.java
@@ -12,8 +12,20 @@
  */
 package org.assertj.swing.test.core;
 
+import org.assertj.swing.annotation.RunsInCurrentThread;
+import org.assertj.swing.annotation.RunsInEDT;
 import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.edt.GuiTask;
+import org.assertj.swing.hierarchy.ComponentHierarchy;
+import org.assertj.swing.hierarchy.ExistingHierarchy;
+import org.junit.After;
 import org.junit.BeforeClass;
+
+import javax.annotation.Nonnull;
+import java.awt.Container;
+import java.awt.Window;
+
+import static org.assertj.swing.edt.GuiActionRunner.execute;
 
 /**
  * Base test case that ensures that Swing components are created and accessed in the EDT.
@@ -21,6 +33,33 @@ import org.junit.BeforeClass;
  * @author Alex Ruiz
  */
 public abstract class EDTSafeTestCase {
+
+  @After
+  public void tearDown() throws Exception {
+    disposeWindows(new ExistingHierarchy());
+  }
+
+  @RunsInEDT
+  private static void disposeWindows(final @Nonnull ComponentHierarchy hierarchy) {
+    execute(new GuiTask() {
+      @Override
+      protected void executeInEDT() {
+        for (Container c : hierarchy.roots()) {
+          if (c instanceof Window) {
+            dispose(hierarchy, (Window) c);
+          }
+        }
+      }
+    });
+  }
+
+  @RunsInCurrentThread
+  private static void dispose(final @Nonnull ComponentHierarchy hierarchy, @Nonnull Window w) {
+    hierarchy.dispose(w);
+    w.setVisible(false);
+    w.dispose();
+  }
+
   @BeforeClass
   public static void setUpOnce() {
     FailOnThreadViolationRepaintManager.install();
